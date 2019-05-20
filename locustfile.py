f986db66a205c087af78c324adc70e4f9c153a32
sofiafernandezmoreno <chui274@gmail.com>
2019-05-20 19:22:50 +0200

    changes in locustfile and docker-compose

--- locustfile.py
+++ locustfile.py
@@ -1,21 +1,22 @@
-#!/usr/bin/python
-
-
 from locust import HttpLocust, TaskSet, task
 
 class UserBehavior(TaskSet):
+    @task()
     def on_start(self):
-      self.login()    
+      self.login() 
+    @task()   
     def login(self):
         self.client.post("/cgi-bin/login.cgi",{"username":"thrukadmin","password":"thrukadmin"})
     
-    @task(4)
+    @task()
     def host(self):
         self.client.get("#cgi-bin/status.cgi?hostgroup=all&amp;style=hostdetail")
-    @task(2)
+    @task()
     def index(self):
         self.client.get("/")
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
     min_wait = 5000
     max_wait = 9000
+
+
